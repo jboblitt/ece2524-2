e019dd0608056a626342dc4915b884f64ca0c153
Patrick To <pkt2@vt.edu>
2012-03-19 02:47:49 -0400

    updated change_grade.py by splitting into a function and a main module

--- change_grade.py
+++ change_grade.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 
 import re
-from sys import argv,exit,stdin,stdout,stderr
 
-if len(argv) < 3:
-    stderr.write("USAGE: change_grade.py <string> <number>\n")
-    exit(1)
-
-target_string = argv[1]
-replace_num = argv[2]
-
-for line in stdin:
-    fields = line.rstrip().split(',')
-    for i in range(0,len(fields)):
+def change_grade(student_string, target_string, replace_num):
+    fields = student_string.rstrip().split(',')
+    for i in range(0, len(fields)):
         found = re.search(target_string, fields[i])
         if found:
             fields[3] = replace_num
-                
-    stdout.write(",".join(fields) + "\n")
-exit(0)
+    return ",".join(fields)
+
+if __name__ == '__main__':
+    from sys import argv,exit,stdin,stdout,stderr
+    
+    if len(argv) < 3:
+        stderr.write("USAGE: change_grade.py <string> <number>\n")
+        exit(1)
+
+    for line in stdin:
+        stdout.write(change_grade(line, argv[1], argv[2]) + "\n")
+    exit(0)
 
